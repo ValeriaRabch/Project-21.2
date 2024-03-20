e92fe1ab980ddf7176dce8660906af4400eb45b1
ValeriaRabch <rabchenukv@gmail.com>
2024-03-20 21:40:40 +0200

    Create file

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,13 @@ using namespace std;
 
 
 int main() {
-	
+	int a; FILE* fres;
+	cout << "Enter mount files"; cin >> a;
+	if (a > 5 || a < 2) {
+		cout << "Error";
+		return 0;
+	}
+
+	fres = fopen("d:\\Valeria\\fres.txt", "w");
 	return 0;
 }
